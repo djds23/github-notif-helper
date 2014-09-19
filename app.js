0daf16ed95233ff05f846e59b51f8d4fca330260
Dean Silfen <dean.silfen@gmail.com>
2014-09-19 10:07:25 -0400

    Remove useless function
    Also fixed using the same counter variable in my neseted for loops. The
    seeker function is fixed, but it does not style the page correctly, see
    inline comment for more detail.

--- app.js
+++ app.js
@@ -1,40 +1,36 @@
-function find_avatars (snippet) {
-    for (i=0; i<snippet.length; i++) {
-        var node = snippet[i];
-        var img = node.getElementsByTagName('img');
-        if (img === false) {
-            continue; 
-        }
-        else {
-            if (img[0].classList.contains("avatar")) {
-                return img[0]; 
-            }
-            else {
-                continue; 
-            }
-        }
-    }
-}
-
 function get_avatars_from_notif (notif) {
-    var avatars = notif.getElementsByClassName('avatar-stack clearfix');
-    return avatars[0].getElementsByTagName('img');
+    if (notif instanceof HTMLElement) {
+        var avatars = notif.getElementsByClassName('avatar-stack clearfix');
+        return avatars[0].getElementsByTagName('img')
+    }
+    else {
+        return false
+    }
 }
 
 function find_notifs_for_user () {
     var user_id = document.getElementsByName("octolytics-actor-id")[0].content;
     var notifications = document.body.getElementsByClassName("boxed-group-inner list-group notifications");
-    for (i=0; i<notifications.length; i++) {
-        var notif = notifications[i];
+    var notifications = notifications[0].childNodes;
+    for (i=0; i<=notifications.length; i++) {
+        console.log(i);
+        console.log(notifications.length)
+        var notif = notifications[i]
         var avatars = get_avatars_from_notif(notif);
-        for (i=0; i<avatars.length; i++) {;
-            if (avatars[i].src.indexOf(user_id) !== -1) {
-                var matched_avatar = avatars[i];
-                var icon_left = notif.parentNode.getElementsByClassName("type-icon octicon");
-                icon_left[0].style.color = "#F2181B";
+        if (avatars) {
+            for (x=0; x<=avatars.length; x++) {
+                var avatar = avatars[x]
+                if (avatar) { 
+                    var new_user_id = avatar.dataset['user']; 
+                    if ( user_id === new_user_id ) {
+                        var icon_left = notif.parentNode.getElementsByClassName("type-icon octicon"); // everything works but this line is wrong, too general. the way it works now you will get an array of all the left icons, it would be better to back out to the 'notif' and slightly changed the background
+                        console.log(icon_left);
+                        icon_left[0].style.color = "#F2181B";
+                    }
+                }
             } 
         }
     }
 }
 
-find_notifs_for_user();
+find_notifs_for_user()
