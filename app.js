308a74bcb57ceb537aa13e6cef48107fb8b36a07
Dean Silfen <dean.silfen@gmail.com>
2014-09-20 08:50:01 -0400

    Updating js to change the background
    Make the background of the notif appear red instead of just having a
    small red icon.

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ function find_notifs_for_user () {
     var notifications = document.body.getElementsByClassName("boxed-group-inner list-group notifications");
     var notifications = notifications[0].childNodes;
     for (i=0; i<=notifications.length; i++) {
-        console.log(i);
-        console.log(notifications.length)
         var notif = notifications[i]
         var avatars = get_avatars_from_notif(notif);
         if (avatars) {
@@ -23,9 +21,10 @@ function find_notifs_for_user () {
                 if (avatar) { 
                     var new_user_id = avatar.dataset['user']; 
                     if ( user_id === new_user_id ) {
-                        var icon_left = notif.parentNode.getElementsByClassName("type-icon octicon"); // everything works but this line is wrong, too general. the way it works now you will get an array of all the left icons, it would be better to back out to the 'notif' and slightly changed the background
-                        console.log(icon_left);
-                        icon_left[0].style.color = "#F2181B";
+                        var icon_left = notif.parentNode.getElementsByClassName("type-icon octicon");
+                        var background = notif.parentNode.getElementsByClassName("js-navigation-item");
+                        background[x].style.backgroundColor = "#FDE5E5";
+                        icon_left[x].style.color = "#F2181B";
                     }
                 }
             } 
