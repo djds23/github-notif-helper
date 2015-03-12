12bee538a6bda104f4a3a06706bc247c6b868579
Dean Silfen <dean.silfen@gmail.com>
2015-03-12 11:01:56 -0400

    Updates to try and get the events to fire on any
    url change.

--- app.js
+++ app.js
@@ -31,25 +31,41 @@ function get_avatars_from_notif (notif) {
     }
 })();
 
-(function () {
+function addToggle(files) {
+    $.each(files, function (i, e) {
+        var action_bar = $(e).find("div.file-actions");
+        
+        if (action_bar.find("#toggle-switch").length) {
+            return
+        }
+        
+        var file_content = $(e).find("div.data");
+        var is_hidden = file_content.is(":visible");
+        var button  = $('<a id="toggle-switch" class="octicon-button tooltipped tooltipped-nw"></a>').clone(); 
+        button.on("click", function () {
+            if (file_content.is(":visible")) {
+                file_content.hide(350); 
+            } else { 
+                file_content.show(350);
+            }
+        });
+        button.appendTo(action_bar);
+        button.attr("aria-label", "Toggle this file");
+        button.html('<span class="octicon octicon-eye"></span>');
+    });
+}
+
+$(document).ready(function (e) {
     var files = $("#files").find("div[id^='diff-']");
     if (files) {
-        $.each(files, function (i, e) {
-            var action_bar = $(e).find("div.file-actions");
-            var file_content = $(e).find("div.data");
-            var is_hidden = file_content.is(":visible");
-            var button  = $('<a class="octicon-button tooltipped tooltipped-nw"></a>').clone(); 
-            button.on("click", function () {
-                if (file_content.is(":visible")) {
-                    file_content.hide(350); 
-                } else { 
-                    file_content.show(350);
-                }
-            });
-            button.appendTo(action_bar);
-            button.attr("aria-label", "Toggle this file");
-            button.html('<span class="octicon octicon-eye"></span>');
-        });
+        addToggle(files);
     }
-})();
 
+    var files_button = $('[data-container-id="files_bucket"')
+    if (files_button) {
+        files_button.on("click", function () {
+            var files = $("#files").find("div[id^='diff-']");
+            addToggle(files);
+        })
+    }
+});
