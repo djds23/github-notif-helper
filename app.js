621ef3eda2fb113ee568a299b1d27cb36734f5c8
Dean Silfen <dean.silfen@gmail.com>
2015-08-08 00:21:58 -0400

    Closes #7 adds toggle all button

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-// Notification Highlighting
-function highlightNotifs () {
-    var userId = $("meta[name=octolytics-actor-id]").attr('content');
-    var notifications = $(".js-notification");
-
-    $.each(notifications, function (_, notif) {
-        var notif = $(notif)
-        var avatars = notif.find('.avatar');
-
-        $.each(avatars, function (_, avatar)  {
-            var newUserId = avatar.dataset.user; 
-            if ( userId === newUserId ) {
-                notif.css("background-color", "#FDE5E5");
-            }
-        }); 
-    });
-}
-
 // File View Toggle
 function getCachedFiles() {
-    return JSON.parse(localStorage.getItem(location.href)) || {}
+    return JSON.parse(localStorage.getItem(location.href)) || {};
 }
 
-function updateLocalStorage(visibilityBool, e) {
-    var jsonViewedFiles = localStorage.getItem(location.href);
-    var viewedFiles = JSON.parse(jsonViewedFiles);
-    var keyId = $(e.toElement).closest("div[id^='diff-']").attr('id');
-    viewedFiles[keyId] = visibilityBool;
-    var jsonViewedFiles = JSON.stringify(viewedFiles);
-    localStorage.setItem(location.href, jsonViewedFiles);
+function getKeyIdFromElement(element) {
+    return $(element.toElement).closest("div[id^='diff-']").attr('id');
+}
+
+function updateLocalStorage(key, value) {
+    var sotredJsonObject = localStorage.getItem(location.href);
+    var pageSpecificJsonCache = JSON.parse(sotredJsonObject);
+    pageSpecificJsonCache[key] = value;
+    var sotredJsonObject = JSON.stringify(pageSpecificJsonCache);
+    localStorage.setItem(location.href, sotredJsonObject);
+}
+
+function addToggleAll(files) {
+    var buttonGroup = $('.btn-group.right');
+    var templateButton = $('<a id="toggle" class="btn btn-sm"></a>');
+    templateButton.html('Toggle All');
+    templateButton.on('click', function (e) {
+        $.each(files, function (i, e) {
+            var fileContent = $(e).find("div.data, div.render-wrapper");
+            toggleVisibility(fileContent);
+        });
+    });
+    templateButton.appendTo(buttonGroup);
 }
 
 function addToggle(files) {
@@ -35,30 +33,21 @@ function addToggle(files) {
     $.each(files, function (i, e) {
         var actionBar = $(e).find("div.file-actions");
         var fileContent = $(e).find("div.data, div.render-wrapper");
-        var isHidden = fileContent.is(":visible");
-   
         var cachedView = viewedFiles[e.id];
-        if (cachedView !== undefined) {
-            if (!cachedView) {
-                fileContent.hide(100);
-            }
+
+        if (cachedView !== undefined && !cachedView) {
+            fileContent.hide(100);
         } else {
             viewedFiles[e.id] = true;
         }
-
         if (actionBar.find("#toggle").length) {
-            return
+            return;
         }
+
         var button  = $('<a id="toggle" class="octicon-btn tooltipped tooltipped-nw"></a>').clone();
         button.on("click", function (e) {
-            var visibilityBool = fileContent.is(":visible");
-            // visibilityBool is negated so we know the state after manipulation 
-            updateLocalStorage(!visibilityBool, e);
-            if (visibilityBool) {
-                fileContent.hide(350); 
-            } else { 
-                fileContent.show(350);
-            }
+            var visibilityBool = toggleVisibility(fileContent); 
+            updateLocalStorage(getKeyIdFromElement(e), visibilityBool);
         });
         button.appendTo(actionBar);
         button.attr("aria-label", "Toggle this file");
@@ -66,6 +55,17 @@ function addToggle(files) {
     });
     var jsonViewedFiles = JSON.stringify(viewedFiles);
     localStorage.setItem(location.href, jsonViewedFiles)
+}
+
+function toggleVisibility(fileContent) {
+    // Toggle visibility and return the new visibility state of the element
+    var visibilityBool = fileContent.is(":visible");
+    if (visibilityBool) {
+        fileContent.hide(350); 
+    } else { 
+        fileContent.show(350);
+    } 
+    return !visibilityBool;
 }
 
 // Trigger an event for location changes since Github does not always
@@ -81,7 +81,6 @@ $(document).ready(function() {
         }
         setTimeout(detectLocationChange, 250);
     }
-    highlightNotifs();
     detectLocationChange();
 });
 
@@ -94,5 +93,7 @@ $(document).on('URL_CHANGE', function () {
     var files = $("#files").find("div[id^='diff-']");
     if (files) {
         addToggle(files);
+        addToggleAll(files);
     }
 });
+
