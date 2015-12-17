bf583e6bc3dc8d8779e5adbef140277bc9caae4b
Dean Silfen <dean.silfen@gmail.com>
2015-12-16 22:05:43 -0500

    Add stubs, include full documentation for utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,30 +6,43 @@ import $ from 'jquery';
  */
 class Utils {
     /**
-     * @return {Map<string, Map<string, boolean>>} map of maps, Url of the PR is
-     *   the key, returns a map of files and their visibility value.
+     * @return {Object<string, boolean>} the ID of each diff and it's visibility bool.
      */
     static getCachedFiles() {
         return JSON.parse(localStorage.getItem(location.href)) || {};
     }
 
-    static getKeyIdFromElement(element) {
-        return $(element.toElement).closest("div[id^='diff-']").attr('id');
+    /**
+     * @param  {MouseEvent} clickEvent - Click event from a file's action bar.
+     * @return {string} ID of the file from the file's Github page.
+     */
+    static getKeyIdFromElement(clickEvent) {
+        return $(clickEvent.toElement).closest("div[id^='diff-']").attr('id');
     }
 
-    static updateLocalStorage(key, value) {
-        if (key === undefined) {
-            console.log('key is undefined');
+    /**
+     * @param  {string} fileId - fileId of the file from the file's Github page.
+     * @param  {boolean} visibilityBool - true if the file should be visible on page load.
+     *   false if the file should be hidden.
+     * @return {boolean} true if the visibility was properly saved.
+     */
+    static updateLocalStorage(fileId, visibilityBool) {
+        if (fileId === undefined) {
+            console.log('fileId is undefined');
             return false;
         }
         let storedJsonObject = localStorage.getItem(location.href);
         let pageSpecificJsonCache = JSON.parse(storedJsonObject);
-        pageSpecificJsonCache[key] = value;
+        pageSpecificJsonCache[fileId] = visibilityBool;
         let sotredJsonObject = JSON.stringify(pageSpecificJsonCache);
         localStorage.setItem(location.href, sotredJsonObject);
         return true;
     }
 
+    /**
+     * @param  {HTMLElement} element - File container from pull page.
+     * @return {jQuery} jQuery div containing the diff for the file.
+     */
     static addToggleButtonForElement(element) {
         let $element = $(element)
         let actionBar = $element.find("div.file-actions");
@@ -39,9 +52,9 @@ class Utils {
         }
 
         let button  = $('<a id="toggle" class="octicon-btn tooltipped tooltipped-nw"></a>');
-        button.on("click", (e) => {
+        button.on("click", (event) => {
             let visibilityBool = Utils.toggleVisibility(fileContent);
-            Utils.updateLocalStorage(Utils.getKeyIdFromElement(e), visibilityBool);
+            Utils.updateLocalStorage(Utils.getKeyIdFromElement(event), visibilityBool);
         });
 
         button.appendTo(actionBar);
@@ -50,6 +63,11 @@ class Utils {
         return fileContent;
     }
 
+    /**
+     * @param {jQuery} fileContent - jQuery div containing the diff for the file.
+     * @return {boolean}  true if the file should be visible on page load.
+     *   false if the file should be hidden.
+     */
     static toggleVisibility(fileContent) {
         // Toggle visibility and return the new visibility state of the element
         let visibilityBool = fileContent.is(":visible");
