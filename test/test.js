19f46f714cbedecc24dd8f32ec4f427970791b9a
Dean Silfen <dean.silfen@gmail.com>
2015-12-18 09:59:30 -0500

    Working tests for toggleVisibility

--- test/test.js
+++ test/test.js
@@ -1,17 +1,39 @@
 import {assert, expect} from 'chai';
 import jsdom from 'mocha-jsdom';
+import simple from 'simple-mock';
 
 import Utils from '../src/utils/utils.js';
-import $ from 'jquery';
 
 
 describe('Utils', function () {
   jsdom()
+
+  before(function () {
+    global.window = document.defaultView
+    global.$ = require('jquery')(window)
+  })
+
+
   describe('#toggleVisibility', function () {
-    it('has document', function () {
-      var div = $(document.createElement('div'));
+
+    it('returns false for a visible element', function () {
+      let div = $(document.createElement('div'));
+      let hide = simple.stub()
+      simple.mock(div, 'is', () => { return true; });
+      simple.mock(div, 'hide', hide);
+
+      expect(Utils.toggleVisibility(div)).to.be.false;
+      expect(hide.callCount).to.eql(1);
+    })
+
+    it('returns true for a hidden element', function () {
+      let div = $(document.createElement('div'));
+      let show = simple.stub()
+      simple.mock(div, 'is', () => { return false; });
+      simple.mock(div, 'show', show);
+
       expect(Utils.toggleVisibility(div)).to.be.true;
-      expect(1).to.equal('true');
+      expect(show.callCount).to.eql(1);
     })
   })
 });
