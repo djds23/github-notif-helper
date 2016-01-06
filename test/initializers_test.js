065f97fef45c2133694981f84cbcd0d1e08eb95a
Dean Silfen <dean.silfen@gmail.com>
2016-01-05 22:26:34 -0500

    New tests for AddToggle

--- test/initializers_test.js
+++ test/initializers_test.js
@@ -8,6 +8,10 @@ import MockLocalStorage from './localStorage.js';
 
 
 describe('Initializers', function () {
+
+  let $;
+  jsdom();
+
   describe('#invalidateCache', function () {
     beforeEach(function () {
       global.localStorage = new MockLocalStorage();
@@ -28,7 +32,7 @@ describe('Initializers', function () {
       let postRequestCache = Utils.getPageCache();
 
       expect(postRequestCache.lastViewed).to.not.be.undefined;
-      expect(postRequestCache.commitNum).to.equal(1);
+      expect(postRequestCache.commitNum).to.eql(1);
       expect(Utils.resetCacheForPage.callCount).to.eql(1);
     })
 
@@ -50,7 +54,7 @@ describe('Initializers', function () {
 
       expect(Initializers.invalidateCache({}, [], 10)).to.be.true;
       expect(Utils.resetCacheForPage.callCount).to.eql(1);
-      expect(Utils.getCachedCommitNumber()).to.equal(10);
+      expect(Utils.getCachedCommitNumber()).to.eql(10);
       expect(Utils.setLastViewed.callCount).to.eql(1);
     })
 
@@ -62,6 +66,57 @@ describe('Initializers', function () {
       expect(Initializers.invalidateCache({}, [], 10)).to.be.false;
       expect(Utils.resetCacheForPage.callCount).to.eql(0);
       expect(Utils.setLastViewed.callCount).to.eql(1);
+    })
+  })
+
+  describe('#addToggleAll', function () {
+    beforeEach(function () {
+      simple.mock(Utils, 'getCachedFiles').callOriginal();
+    })
+
+    it('does nothing if no files are passed', function () {
+      Initializers.addToggleAll({}, [], 0)
+      expect(Utils.getCachedFiles.callCount).to.eql(0)
+    })
+  })
+
+  describe('#addToggle', function () {
+    beforeEach(function () {
+      global.window = document.defaultView;
+      global.$ = require('jquery')(window);
+      simple.mock(Utils, 'addToggleButtonForElement').returnWith(undefined);
+      simple.mock(Utils, 'getCachedFiles').callOriginal();
+
+      Array.prototype.each = function (func) {
+        this.forEach((element, index, array) => {
+          func(index, element);
+        });
+      };
+    })
+
+    it('does nothing if no files are passed', function () {
+      Initializers.addToggle({}, [], 0)
+      expect(Utils.getCachedFiles.callCount).to.eql(0)
+    })
+
+    it('adds id to cache for each file object', function () {
+      const mockFileOne = { id: 'diff-0' };
+      const mockFileTwo = { id: 'diff-1' };
+      const mockFileThree = { id: 'diff-2' };
+
+      let callToggle = (() => {
+        Initializers.addToggle({}, [mockFileOne, mockFileTwo, mockFileThree], 1)
+      })
+
+      let cachedFilesFunc = Utils.getCachedFiles;
+      let expectedOutput = {
+        'diff-0': true,
+        'diff-1': true,
+        'diff-2': true
+      };
+
+      expect(callToggle).to.increase(cachedFilesFunc, 'callCount');
+      expect(Utils.getCachedFiles()).to.eql(expectedOutput);
     })
   })
 });
