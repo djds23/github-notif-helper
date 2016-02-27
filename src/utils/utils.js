a1ec2f573a3dea1a8b689e3893e81e83994a5af8
Dean Silfen <dean.silfen@gmail.com>
2016-02-27 12:43:01 -0500

    Update icon to SVG

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -106,7 +106,18 @@ class Utils {
 
         button.appendTo(actionBar);
         button.attr("aria-label", "Toggle this file");
-        button.html('<span class="octicon octicon-eye"></span>');
+        button.html(`
+          <svg
+            aria-hidden="true"
+            class="octicon octicon-eye"
+            height="16"
+            role="img"
+            version="1.1"
+            viewBox="0 0 14 16" width="18">
+              <path d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6c4.94 0 7.94-6 7.94-6S13 2 8.06 2z m-0.06 10c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4z m2-4c0 1.11-0.89 2-2 2s-2-0.89-2-2 0.89-2 2-2 2 0.89 2 2z">
+              </path>
+          </svg>
+        `);
         return fileContent;
     }
 
